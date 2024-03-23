8c918711a99961dc573773a27f264d01edc3210f
jycao <caojiayu9@gmail.com>
2024-03-23 11:31:36 +0800

    修改了parzen窗的控制线

--- parzen.py
+++ parzen.py
@@ -6,7 +6,7 @@ train_data = np.genfromtxt('data/Train_data.csv', delimiter=',')
 test_data1 = np.genfromtxt('data/Test_data1.csv', delimiter=',')
 test_data2 = np.genfromtxt('data/Test_data2.csv', delimiter=',')
 
-bandwidth = 1.0  # 窗宽度参数
+bandwidth = 1.0
 parzen_estimator = KernelDensity(bandwidth=bandwidth)
 parzen_estimator.fit(train_data)
 
@@ -14,7 +14,7 @@ log_density = parzen_estimator.score_samples(train_data)
 log_density1 = parzen_estimator.score_samples(test_data1)
 log_density2 = parzen_estimator.score_samples(test_data2)
 
-threshold = np.percentile(sorted(log_density), 0)  # 以5%分位数作为阈值
+threshold = np.percentile(sorted(log_density), 0)
 
 def detect_anomalies(log_density, threshold):
     currect = 0.0
