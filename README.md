1370ee77b335e9ae2b18462bf811e690d2f3e08d
jycao <caojiayu9@gmail.com>
2024-03-23 12:39:31 +0800

    增加了注释和自述
    在parzen窗方法中增加了注释，并在README中写了环境安装方法

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+## 使用
+所需环境：anaconda/miniconda
+1. 创建虚拟环境
+```
+conda create -n oc python=3.9
+```
+2. 进入虚拟环境
+```
+conda activate oc
+```
+3. 安装依赖
+```
+pip install -r requirements.txt
+```
+4. 运行
+```
+python mixture.py
+python norm.py
+python svm.py
+python parzen.py
+```
+
+## 部分参考
 单分类算法：One Class SVM:
 https://blog.csdn.net/qq_19446965/article/details/118742147
 
--- parzen.py
+++ parzen.py
@@ -2,10 +2,12 @@ from sklearn.neighbors import KernelDensity
 import numpy as np
 import matplotlib.pyplot as plt
 
+# 读取数据集
 train_data = np.genfromtxt('data/Train_data.csv', delimiter=',')
 test_data1 = np.genfromtxt('data/Test_data1.csv', delimiter=',')
 test_data2 = np.genfromtxt('data/Test_data2.csv', delimiter=',')
 
+# 使用Parzen窗法进行拟合
 bandwidth = 1.0
 parzen_estimator = KernelDensity(bandwidth=bandwidth)
 parzen_estimator.fit(train_data)
@@ -14,26 +16,31 @@ log_density = parzen_estimator.score_samples(train_data)
 log_density1 = parzen_estimator.score_samples(test_data1)
 log_density2 = parzen_estimator.score_samples(test_data2)
 
+# 使用拟合好的Parzen窗法来评估测试样本的密度
 threshold = np.percentile(sorted(log_density), 0)
 
+# 计算异常值的函数
 def detect_anomalies(log_density, threshold):
     currect = 0.0
     for i in range(0, 4000):
         if i < 2000:
+            # 高于控制线的正常值计数
             if log_density[i] >= threshold:
                 currect += 1
         else:
+            # 低于控制线的异常值计数
             if log_density[i] < threshold:
                 currect += 1
     return currect/4000
 
+# 得到最终正确率
 currency_test1 = detect_anomalies(log_density1, threshold)
 print("Currency in Test Data 1:", currency_test1)
 
 currency_test2 = detect_anomalies(log_density2, threshold)
 print("Currency in Test Data 2:", currency_test2)
 
-
+# 绘图
 plt.subplot(1, 2, 1)
 plt.bar(range(1,4001),log_density1)
 plt.title("data analyze")
